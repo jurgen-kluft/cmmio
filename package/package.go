8aa81e333616d4529d6805808f060803e63c612b
jurgen.kluft <jurgen.kluft@gmail.com>
2026-04-21 20:50:04 +0800

    * cmmio
    * update package.go

--- package/package.go
+++ package/package.go
@@ -32,7 +32,6 @@ func GetPackage() *denv.Package {
 	// test library
 	testlib := denv.SetupCppTestLibProject(mainpkg, name)
 	testlib.AddDependencies(ccorepkg.GetTestLib())
-	testlib.AddDependencies(cunittestpkg.GetTestLib())
 
 	// unittest project
 	maintest := denv.SetupCppTestProject(mainpkg, name)
